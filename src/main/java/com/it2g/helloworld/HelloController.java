e926460c2a9ead5a6dcd417a89ca269e6193d3c3
mikhailov <mikhailov@mailkenk.ru>
2018-04-19 09:21:06 +0700

    Добавлен простой GET-метод, возвращающий JSON.

--- src/main/java/com/it2g/helloworld/HelloController.java
+++ src/main/java/com/it2g/helloworld/HelloController.java
@@ -1,7 +1,13 @@
 package com.it2g.helloworld;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
 public class HelloController {
@@ -9,5 +15,15 @@ public class HelloController {
     @RequestMapping("/")
     public String index() {
         return "Hello World!";
+    }
+
+    @RequestMapping(
+            value = "/api/product",
+            method = {RequestMethod.GET},
+            params = {"id"},
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Map GetProduct(@RequestParam(value = "id") int id) {
+        return Collections.singletonMap("response", id);
     }
 }
